9f207710dc26e5d9f2adafbd9b015b6a1ce4abc7
ShunTr <repaco9@gmail.com>
2024-10-11 10:15:11 +0200

    Loading and error state

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,13 +1,18 @@
 import useHttp from '../hooks/useHttp.jsx'
+import Error from './Error.jsx'
 import MealItem from './MealItem.jsx'
 
 const requestConfig = {} // create this to avoid infinite loop
 
 export default function Meals() {
-    const { data: loadedMeals, isLoading } = useHttp('http://localhost:3000/meals', requestConfig, [])
+    const { data: loadedMeals, isLoading, error } = useHttp('http://localhost:3000/meals', requestConfig, [])
 
     if (isLoading) {
-        return <p>Loading...</p>
+        return <p className="center">Loading...</p>
+    }
+
+    if (error) {
+        return <Error title="Error" message={error} />
     }
 
     return (
